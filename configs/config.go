e1c3b6b1f8ed143b0c068bfac913d9e20c73e869
Luciano Santos <luciano_z7@hotmail.com>
2024-01-22 22:47:51 -0300

    Adding rate limiter structure

---
+++ configs/config.go
@@ -0,0 +1,51 @@
+package configs
+
+import "github.com/spf13/viper"
+
+type conf struct {
+	DBDriver   string `mapstructure:"DB_DRIVER"`
+	DBHost     string `mapstructure:"DB_HOST"`
+	DBPort     string `mapstructure:"DB_PORT"`
+	DBPassword string `mapstructure:"DB_PASSWORD"`
+	TokenLimit int    `mapstructure:"TOKEN_LIMIT"`
+	IPLimit    int    `mapstructure:"IP_LIMIT"`
+}
+
+type Conf struct {
+	TokenLimit int `mapstructure:"TOKEN_LIMIT"`
+	IPLimit    int `mapstructure:"IP_LIMIT"`
+}
+
+func LoadConfig(path string) (*conf, error) {
+	var c *conf
+	viper.AddConfigPath(path)
+	viper.SetConfigName("config")
+	viper.SetConfigType("env")
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	err = viper.Unmarshal(&c)
+	if err != nil {
+		panic(err)
+	}
+	return c, err
+}
+
+func GetTokenLimit() int {
+	c, err := LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	return c.TokenLimit
+}
+
+func GetIpLimit() int {
+	c, err := LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	return c.IPLimit
+}
--- pkg/rateLimiter/rateLimiter.go
+++ pkg/rateLimiter/rateLimiter.go
@@ -1,1 +1,89 @@
 package ratelimiter
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	entity "github.com/GoExpertCurso/GoRateLimiterFC/internal/entity"
+	"github.com/go-redis/redis"
+)
+
+type RateLimiter struct {
+	client *redis.Client
+	limit  int
+	window time.Duration
+}
+
+func NewRateLimiter(client *redis.Client, limit int, window time.Duration) *RateLimiter {
+	return &RateLimiter{
+		client: client,
+		limit:  limit,
+		window: window,
+	}
+}
+
+func (rl *RateLimiter) Allow(ipOrToken string, request entity.Request) bool {
+	key := ipOrToken
+	now := time.Now().Unix()
+
+	res, _ := rl.client.TxPipelined(func(p redis.Pipeliner) error {
+		p.HIncrBy(key, "count", 1)
+		p.Expire(key, rl.window)
+		p.HGet(key, "timestamp")
+		return nil
+	})
+
+	if len(res) < 3 {
+		fmt.Println("Resposta do pipeline menor do que o esperado")
+		return false
+	}
+
+	count := rl.getCount(ipOrToken)
+
+	timestamp, _ := res[2].(*redis.StringCmd).Int64()
+
+	fmt.Printf("key: %s\n", key)
+
+	if now-int64(rl.window.Seconds()) > timestamp {
+		rl.client.HSet(key, "timestamp", now)
+		rl.client.HSet(key, "count", 1)
+		return true
+	}
+
+	if count > int64(rl.limit) {
+		return false
+	}
+
+	return true
+}
+
+func (rl *RateLimiter) Block(ipOrToken string, limit int64) bool {
+	key := ipOrToken
+	count := rl.getCount(key)
+
+	return count > limit
+}
+
+func (rl *RateLimiter) getCount(ipOrToken string) int64 {
+	hashKey := ipOrToken
+	field := "count"
+
+	// Contexto para operações Redis
+	//ctx := context.Background()
+
+	// Obter o valor do campo no hash
+	val, err := rl.client.HGet(hashKey, field).Result()
+	if err == redis.Nil {
+		fmt.Println("Campo não encontrado")
+	} else if err != nil {
+		fmt.Println("Erro:", err)
+	} else {
+		fmt.Println("Valor:", val)
+		fmt.Println()
+	}
+
+	result, _ := strconv.ParseInt(val, 10, 64)
+
+	return result
+}
